bade1e7ba943c0b183ea1d4a26aa40923c52e76f
AdamSai <adamsaidane252@gmail.com>
2020-11-05 20:33:05 +0100

    parser

---
+++ DM_State/AstTree.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DM_State
+{
+    class AstTree
+    {
+        private AstTree left;
+        private AstTree right;
+    }
+}
---
+++ DM_State/VsslParser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DM_State
+{
+    class VsslParser
+    {
+
+
+        public AstTree VsslToAst(string path)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
